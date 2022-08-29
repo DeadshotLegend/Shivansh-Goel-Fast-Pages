16f193a61c2fed914e177947a5b5dbcaf94bf5a4
deadshotlegend <shivanshggamer@gmail.com>
2022-08-28 18:56:52 -0700

    Trying to hide the code cell

--- python/quiz.py
+++ python/quiz.py
@@ -2,37 +2,34 @@ import getpass, sys
 
 def question_with_response(prompt):
     print("Question: " + prompt)
-    return input()
+    msg = input()
+    return msg
 
-# create a dictionary (key value) of questions and answers
-# this way I can add as many questions as I want without duplicating the code
-quesstionsAndAsnwers = {
-  "What command is used to include other functions that were previously developed?": "import",
-  "What command is used to evaluate correct or incorrect response in this example?": "if",
-  "Each 'if' command contains an '_________' to determine a true or false condition?": "expression"
+questionsDict = {"What command is used to include other functions that were previously developed?": "import",
+    "What command in this example is used to evaluate a response?": "if", 
+    "Each 'if' command contains an '_________' to determine a true or false condition?": "expression",
+    "What does Jupyter Notebooks use": "python",
+    "What is the end of the file name of Mark Down Post": ".md",
+    "What kernel does one need to use to use python": "ipykernel",
+    "What command in terminal allows one to switch directort": "cd"
 }
-
-# since the number of questions can change, the questions variable is populated with the length of the dictionary
-questions = len(quesstionsAndAsnwers)
+questions = len(questionsDict)
 correct = 0
+
 
 print('Hello, ' + getpass.getuser() + " running " + sys.executable)
 print("You will be asked " + str(questions) + " questions.")
-print("Are you ready to take a test! Hit Enter to begin.")
+print("Are you ready to take a test! Press Enter key to begin. Best of luck :)")
 input()
 
-# iterate over the keys from the dictionary
-for key in quesstionsAndAsnwers:
-    # pass the question (key) to the function
+for key in questionsDict:
     rsp = question_with_response(key)
-    # compare the user response with value from the dictionary (the answer)
-    if rsp == quesstionsAndAsnwers[key]:
-        print(rsp + " is correct! Wohoo!")
+    if rsp.lower() == questionsDict[key].lower():
+        print(rsp + " is correct! Good Job!")
         correct += 1
     else:
-        print(rsp + " is incorrect! Better luck next time :(")
-
-print(getpass.getuser() + " you scored " + str(correct) + "/" + str(questions))
-#print the percentage
-print("Total Percentage: " + str (format(correct/questions * 100,".2f")) + "%")
-
+        print(rsp + " is incorrect! Better Luck next time.")
+    
+print(getpass.getuser() + " you scored " + str(correct) +"/" + str(questions))
+page = correct/questions * 100
+print("Total Percentage: " + str (format(page,".2f")) + "%")
